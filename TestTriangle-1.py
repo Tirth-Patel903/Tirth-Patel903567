3ad876d65844feb3e3500ef1e4afecb98651b7b8
Tirth-Patel903 <99315530+Tirth-Patel903@users.noreply.github.com>
2023-02-18 23:48:35 +0530

    Revert "This is updated code solved some errors."
    This reverts commit 801893f02434943946de06b1da4f91343e47ec39.

--- TestTriangle-1.py
+++ TestTriangle-1.py
@@ -11,18 +11,14 @@ import unittest
 
 from Triangle import classifyTriangle
 
-#from Triangle import classifyTriangle
-
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
-
-
 
 class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
  
     def testInvalidInput(self):
-        self.assertEqual(classifyTriangle(6, 2, 3), 'Test1')
+        self.assertEqual(classifyTriangle(-1, 2, 3), 'Test1')
         self.assertEqual(classifyTriangle(201, 202, 203), 'Test2')
         self.assertEqual(classifyTriangle(1.5, 2, 3), 'Test3')
 
@@ -34,13 +30,13 @@ class TestTriangles(unittest.TestCase):
         
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
-
+	
     def testScaleneTriangle(self):
         self.assertEqual(classifyTriangle(2, 3, 4), 'Scalene', '2, 3, 4 should be scalene')
-
+    
     def testIsocelesTriangle(self):
         self.assertEqual(classifyTriangle(2, 2, 3), 'Isoceles', '2, 2, 3 should be isoceles')
-
+    
     def testTriangleInequality(self):
         self.assertEqual(classifyTriangle(1, 2, 4), 'NotATriangle', '1, 2, 4 should not be a triangle')
 
